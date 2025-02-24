e512070ad0a43de02f408f2f84ec5a284531fe93
Joyce Harvatovici <joyce.harvatovici@gmail.com>
2025-02-25 00:20:15 +0200

    working on git-pages config and build file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+        filename: 'build.js'
     },
     module: {
         rules: [{
